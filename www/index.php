5ff5c94ac89cb1f3e4a6b42b3513b966629a9f9a
romanenko123 <romaromro@gmail.com>
2016-03-31 15:42:09 +0300

    first commit for starting

--- www/index.php
+++ www/index.php
@@ -1,26 +1,2 @@
 <?php
-session_start(); // стартуем сессию
-
-// если в сессии нет массива корзины то создаем его
-if (! isset($_SESSION['cart'])) {
-    $_SESSION['cart'] = array();
-}
-
-include_once '../config/config.php';         // Инициализация настроек
-include_once '../config/db.php';             // Инициализация БД
-include_once '../library/mainFunctions.php'; // Основные функции
-
-// определяем с каким контроллером будем работать
-$controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : "Index";
-
-// определяем с какой функцией будем работать
-$actionName = isset($_GET['action']) ? $_GET['action'] : "index";
-
-if (isset($_SESSION['user'])) {
-    $smarty->assign('arUser', $_SESSION['user']);
-}
-
-//иницифлизируем переменную шаблонизатора количества элементов в корзине
-$smarty->assign('cartCntItems', count($_SESSION['cart']));
-
-loadPage($smarty, $controllerName, $actionName);
+    phpinfo();
