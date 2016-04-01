82dd62608ba43d5824e7d5ca33c6a59689b512e6
romanenko123 <romaromro@gmail.com>
2016-04-01 13:05:13 +0300

    add Smarty template engine

--- config/config.php
+++ config/config.php
@@ -7,3 +7,27 @@
 // определяем константы для определения контроллера
 define("PathPrefix", '../controllers/');
 define("PathPostfix", 'Controller.php');
+
+//> ипользуем шаблон
+$template = 'default';
+
+// пути к файлам шаблонов (*.tpl)
+define('TemplatePrefix', "../views/{$template}");
+define('TemplatePostfix', ".tpl");
+
+// пути к файлам шаблонов в вебпространстве
+// define('TemplateWebPath', "/templates/{$template}/");
+//<
+
+//> Инициализация шаблонизатора Smarty
+// put full path to Smarty.class.php
+require('../library/Smarty/libs/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir(TemplatePrefix);
+$smarty->setCompileDir('../tmp/smarty/templates_c');
+$smarty->setCacheDir('../tmp/smarty/cache');
+$smarty->setConfigDir('../library/Smarty/configs');
+
+// $smarty->assign('teplateWebPath', TemplateWebPath);
+//<
--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -7,3 +7,15 @@ function testAction()
 {
     echo "это tect action from index controller";
 }
+
+/**
+ * Формирование главной стр сайта
+ * 
+ * @param object $smarty шаблонизатор
+ */
+function indexAction($smarty)
+{
+    $smarty->assign('pageTitle', "Главная стр сайта");
+    
+    loadTemplate($smarty, "index");
+}
