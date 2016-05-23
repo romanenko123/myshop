e53fcfffe1a4cce7e3f8655828bbee5ba38346b0
romanenko123 <romaromro@gmail.com>
2016-05-23 13:25:50 +0300

    add two value for entity order

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -11,6 +11,77 @@ include_once '../models/CategoriesModel.php';
 include_once '../models/ProductsModel.php';
 
 /**
+ * Формирование стр заказы
+ * 
+ * @param obj $link линк для бд
+ * @param object $smarty шаблонизатор
+ */
+function orderAction($link, $smarty)
+{
+    //  получаем массив идентификаторов (ID) продуктов корзины
+    $itemIds = isset($_SESSION['cart']) ? $_SESSION['cart'] : null;
+    //  если корзина пуста то редиректим в корзину
+    if (! $itemIds) {
+        redirect("/cart/");
+        return ;
+    }
+    //  получаем из массива $_POST количество покупаемых товаров
+    $itemsCnt = array();
+    foreach ($itemIds as $item) {
+        // формируем ключ для массива POST
+        $postVar = "itemCnt_".$item;
+        // создаем элемент массива количества покупаемого товара
+        // ключ массива - ID товара, значение массива - количество товара
+        // $itemsCnt[1] = 3; товар с ID == 1 покупают 3 шт
+        $itemsCnt[$item] = isset($_POST[$postVar]) ? $_POST[$postVar] : null;
+    }
+    $resultProducts = getProductsFromArray($link, $itemIds);
+    
+    // добовляем каждому продукту дополнительное поле
+    // "realPrice" = количество продуктов * на цену продукта
+    // "cnt" = количество покупаемого товара
+    
+    // &$item - для того чтобы при изменении переменной $item
+    // менялся и элемент массива $resultProduct
+    $i = 0;
+    foreach ($resultProducts as &$item) {
+        $item['cnt'] = isset($itemsCnt[$item['id']]) ? $itemsCnt[$item['id']] : 0;
+        if ($item['cnt']) {
+            $item['realPrice'] = $item['cnt'] * $item['price'];
+        } else {
+            // если вдруг получилось так что товар в корзине есть
+            // а количество == 0, то удаляем этот товар
+            unset($resultProducts[$i]);
+        }
+        $i++;
+    }
+    
+    if (! $resultProducts) {
+        echo "Корзина пуста";
+        return ;
+    }
+    
+    // полученный массив покупаемых товаров помещаем в ссесию
+    $_SESSION["saleCart"] = $resultProducts;
+    
+    $resultCategories = getAllMainCatsWithChildren($link);
+    
+    // hideLoginBox переменная - флаг для того чтобы спрятать 
+    // блоки логина и регистрации в боковой панели
+    if (! isset($_SESSION["user"])) {
+        $smarty->assign("hideLoginBox", true);
+    }
+    
+    $smarty->assign("pageTitle", "Заказ");
+    $smarty->assign("resultCategories", $resultCategories);
+    $smarty->assign("resultProducts", $resultProducts);
+    
+    loadTemplate($smarty, "header");
+    loadTemplate($smarty, "cart");
+    loadTemplate($smarty, "footer");
+}
+
+/**
  * Формирование стр корзины
  * 
  * @param obj $link линк для бд
